41d71cf4642ffee323743e73260787995971bfa6
Thomas Ricatte <thomas@ricatte.fr>
2025-02-06 21:13:19 +0100

    Initial CDK commit

---
+++ test/tricatte-perso-web-cdk.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as TricattePersoWebCdk from '../lib/tricatte-perso-web-cdk-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/tricatte-perso-web-cdk-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new TricattePersoWebCdk.TricattePersoWebCdkStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
